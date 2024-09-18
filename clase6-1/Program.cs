55b2acccf5bd7987f4543e33b796478231939573
Marl0023 <marlotay2312@gmail.com>
2024-09-18 08:36:25 -0500

    first save

---
+++ clase6-1/Program.cs
@@ -0,0 +1,15 @@
+﻿int suma = 0;
+do {
+    suma = 0;
+    Console.WriteLine("Ingrese un numero: ");
+    int num= int.Parse(Console.ReadLine());
+    for (int i = 1; i <= num; i++) {
+        suma += i;
+    }
+    Console.WriteLine($"La suma desde el numero  1 hasta el numero {num}: {suma}");
+    Console.WriteLine("Desea continuar ? (S/N)");
+    char resp = char.Parse(Console.ReadLine().ToUpper());
+    if (resp == 'N') {
+        break;
+    }
+}while (true);
